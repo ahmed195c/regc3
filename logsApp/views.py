013fc67efdb1ec39daf45a1013066b2130ef9e1c
ahmedjhin <a7madti90@hotmail.com>
2024-09-26 13:05:05 +0400

    cheack if data well be lost 1

--- logsApp/views.py
+++ logsApp/views.py
@@ -32,9 +32,8 @@ def registerCar(request):
         except EmployesInfo.DoesNotExist:
             empDoseNotEXISTS = "الرقم الاداري غير صحصح او مستخدم"
             return render(request, "logsApp/registerCar.html",{"empDoseNotEXISTS":empDoseNotEXISTS,"l":allnUseCars})
-        print(" testif data from lab will push to work pc  ")
-        print("commit 1 to test if  local repo will get a conflict for being delayed")
-        print("second commit to see if local repo will get a conflict ")
+        print("commit 1 check for data")
+
         try:
             carExists = RegistredCars.objects.get(carNumber=carNInput,carIsInparking=True)
         except RegistredCars.DoesNotExist:
